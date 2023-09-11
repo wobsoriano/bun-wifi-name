ce3ba138f25e13a44c819f9f50a5aaed2b045448
wobsoriano <sorianorobertc@gmail.com>
2023-09-11 14:14:56 -0700

    update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,7 +2,5 @@ import { expect, test } from 'bun:test'
 import { getWifiName } from '../src'
 
 test('getWifiName', () => {
-  // Wifi name is unique so...
-  const notEmpty = getWifiName() !== ''
-  expect(notEmpty).toBe(true)
+  expect(getWifiName()).toBeTruthy()
 })
